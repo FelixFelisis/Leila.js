eeefe86289e262ed0d0337ece883aae9f408a426
jdrprod <jdrprod@gmail.com>
2018-03-31 23:39:29 +0200

    base

---
+++ src/Leila.js
@@ -0,0 +1,18 @@
+
+window.Leila = {};
+
+Leila.Game = function() {
+  this.hello = "hello world"
+}
+
+Leila.Game.prototype.test = function() {
+  console.log(this.hello);
+}
+
+function leila() {
+  return new Game();
+}
+
+var app = leila();
+
+app.test();
